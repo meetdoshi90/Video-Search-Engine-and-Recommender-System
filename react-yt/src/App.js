9aa9b12806d56c1d596ea895bddf248d45b4e9ca
meetdoshi90 <meetdoshi90@gmail.com>
2021-08-23 06:31:06 +0530

    Changes

--- react-yt/src/App.js
+++ react-yt/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Enter search</div>
+        <form action="{{url_for('recommend')}}">
+        <input type="text" placeholder="Youtube search" name="youtube" id="complete"/>  
+        </form>
+        <br></br>
+        <button onClick="loadVideos()" type='submit' class="btn btn-primary">Search</button>
       </header>
     </div>
   );
 }
-
 export default App;
